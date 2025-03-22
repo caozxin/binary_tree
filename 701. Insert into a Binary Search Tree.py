a9236385e864fb2a4a937bf11b9fb2e1f7f4dade
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-03-22 06:43:55 -0400

    Update 701. Insert into a Binary Search Tree.py

--- 701. Insert into a Binary Search Tree.py
+++ 701. Insert into a Binary Search Tree.py
@@ -8,19 +8,19 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+    def insertIntoBST(self, root: TreeNode, target: int) -> TreeNode:
         if not root:
-            return TreeNode(val) # this is our exsit from recursion call! 
+            return TreeNode(target) # this is our exsit from recursion call! 
 
-        if root.val < val:
+        if root.val < target:
             if not root.right: # if the node has no right child and the node.val < target val, TreeNode(val) should be its right child
-                root.right = TreeNode(val)
+                root.right = TreeNode(target)
             else: # if the node has another right child, we will continue searching 
-                self.insertIntoBST(root.right, val)
+                self.insertIntoBST(root.right, target)
         else:
             if not root.left: # if the node has no left child and node.val > target.val, TreeNode(val) should be its left child
-                root.left = TreeNode(val)
+                root.left = TreeNode(target)
             else:# if the node has another right child, we will continue searching 
-                self.insertIntoBST(root.left, val)
+                self.insertIntoBST(root.left, target)
 
         return root # here we return the updated node from child to parent
