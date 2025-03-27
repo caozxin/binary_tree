71871d2d16b7e6b8d15d7bccb8d0eb736195aa7f
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-03-27 06:39:21 -0400

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 236. Lowest Common Ancestor of a Binary Tree.py
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -7,28 +7,22 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs_checks(node, p, q, if_p_q):
-            if not node:
+        def dfs_checks(node, p, q):
+            if not node: # exit strategy/base case if we reach leaf node
                 return None
             
-            # # If both p and q are smaller, LCA must be in the left subtree
-            # if node.val == p.val:
-            #     if_p_q[0] = 'true'
-            # elif node.val == q.val:
-            #     if_p_q[1] = 'true'
+            # If root matches either p or q, return root
+            if node == p or node == q:
+                return node
             
-            left = dfs_checks(node.left, p, q, if_p_q)
-            right = dfs_checks(node.right, p, q, if_p_q)
+            left = dfs_checks(node.left, p, q)
+            right = dfs_checks(node.right, p, q)
+
+            # If both left and right are not null, root is the LCA
+            if left and right:
+                return node
             
-            # If we reach here, it means node is the split point (LCA)
-            if node.val == p.val:
-                if_p_q[0] = 'true'
-            elif node.val == q.val:
-                if_p_q[1] = 'true'
-            print(node.val, if_p_q)
+            # If only one side returns a valid node, return that node
+            return left if left else right
 
-            return node 
-
-        if_p_q = ['false', 'false']
-
-        return dfs_checks(root, p, q, if_p_q)
+        return dfs_checks(root, p, q)
